b40f4fac12c6e5810e303c8b403eb49004de85c1
tom mcandrew <mcandrew@lehigh.edu>
2023-01-25 22:59:21 -0500

    update

--- training_SEIR.py
+++ training_SEIR.py
@@ -39,8 +39,8 @@ if __name__ == "__main__":
         return np.stack([ds_dt,de_dt,di_dt,dh_dt,dr_dt, dc_dt])
 
     N  = 12*10**6
-    I0 = 5./N
     
+    I0 = 5./N
     E0 = I0
     
     ps = 0.10
@@ -61,30 +61,31 @@ if __name__ == "__main__":
     cum_hosps     = states[:,-1]
     inc_hosps     = np.clip(np.diff(cum_hosps)*N, 0, np.inf)
 
-    noisy_hosps = dist.NegativeBinomial2( inc_hosps, 5.95 ).sample(rng_key).to_py()
+    noisy_hosps = np.asarray(dist.NegativeBinomial2( inc_hosps, 5.95 ).sample(rng_key))
 
     time_at_peak = np.argmax(inc_hosps)
 
     #--give training data
-    pct = 1.5
+    pct = 0.50
     train_N = int(time_at_peak*pct)
 
     training_data =noisy_hosps[:train_N]
 
-
     #--model
     
-    def model(training_data,ttl, ps):
+    def model(training_data,ttl, ps, times):
 
         #--derive from data
         T = len(training_data)
+        times = jnp.arange(0,times)
         
-        sigma = numpyro.sample("sigma", dist.Gamma( 1/7., 1. ) )
-        gamma = numpyro.sample("gamma", dist.Gamma( 1/7., 1. ) )
-        kappa = numpyro.sample("kappa", dist.Gamma( 1/7., 1. ) )
-        phi   = 0.025#numpyro.sample("phi", dist.Beta( 20*(0.10), 20*(1 - 0.80) ) )
+        sigma = numpyro.sample("sigma", dist.Gamma( 1/2., 1. ) )
 
-        r0  = numpyro.sample("R0", dist.Gamma( 1., 1. ) )
+        gamma = 1./2
+        kappa = 1./7 
+        phi   = 0.025 
+
+        r0  = numpyro.sample("R0", dist.Uniform(0.75,4))
 
         beta = r0*gamma*(1./ps)
 
@@ -116,32 +117,32 @@ if __name__ == "__main__":
         H0 = 0
         R0 = 1. - S0 - I0 - E0
         
-        final, states = jax.lax.scan( lambda x,y: evolve(x,y, (sigma, beta, gamma,kappa) ), jnp.vstack( (S0,E0,I0,H0,R0, H0) ), jnp.arange(T))   
+        final, states = jax.lax.scan( lambda x,y: evolve(x,y, (sigma, beta, gamma,kappa) ), jnp.vstack( (S0,E0,I0,H0,R0, H0) ), times)   
 
         #--sim
         states = numpyro.deterministic("states",states)
         
-        inc_hosps = states[:,-1]*ttl
+        inc_hosps = (states[:,-1]*ttl).reshape(-1,)
         inc_hosps = numpyro.deterministic("inc_hosps",inc_hosps)
-        
-        sim = numpyro.sample("sim_inc_hosps", dist.NegativeBinomial2(inc_hosps, 2.5), obs = training_data)
+
+        sim = numpyro.sample("sim_inc_hosps", dist.NegativeBinomial2(inc_hosps[:T], 10), obs = training_data)
 
     nuts_kernel = NUTS(model)
     mcmc        = MCMC( nuts_kernel , num_warmup=1500, num_samples=2000,progress_bar=True)
 
-
-    mcmc.run(rng_key, extra_fields=('potential_energy',), training_data = training_data, ttl = N, ps = ps)
+    mcmc.run(rng_key, extra_fields=('potential_energy',), training_data = training_data, ttl = N, ps = ps, times = 500)
     mcmc.print_summary()
     samples = mcmc.get_samples()
 
     #--plot
+    plt.scatter(times[1:train_N], noisy_hosps[1:train_N], lw=1, color="blue", alpha=1,s=3)
+    plt.scatter(times[train_N+1:] , noisy_hosps[train_N:], lw=1 , color="black", alpha=1,s=3)
     
-    plt.scatter(times[1:], noisy_hosps, lw=1, color="blue", alpha=1,s=3)
     plt.plot(times[1:], inc_hosps, color="black")
 
     predicted_inc_hosps = samples["inc_hosps"].mean(0)
 
-    plt.plot(predicted_inc_hosps, color= "red")
+    plt.plot(predicted_inc_hosps, color= "red", ls='--')
     
     plt.show()
 
