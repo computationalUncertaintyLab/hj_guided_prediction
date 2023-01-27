6f720e9502bcdf0e6afa6b5367d1b954d8e7a5c8
tom mcandrew <mcandrew@lehigh.edu>
2023-01-26 22:38:51 -0500

    update

--- training_SEIR.py
+++ training_SEIR.py
@@ -19,9 +19,19 @@ from jax import random
 import jax.numpy as jnp
 
 
-if __name__ == "__main__":
 
-    rng_key     = random.PRNGKey(0)
+def generate_data( N = 12*10**6        #--population size 
+                  ,I0 = 5./(12*10**6)  #--initial proportion of infectors
+                  ,E0 = 5./(12*10**6)  #--initial proportion of exposed
+                  ,H0 = 0              #--initial proportion of hospitalized
+                  ,T      = 500           #--total number of time steps to observe outbreak
+                  ,ps     = 0.10         #--proportion of susceptible in the population
+                  ,sigma  = 1./2
+                  ,r0     = 1.4
+                  ,gamma  = 1/.2
+                  ,kappa  = 1./7
+                  ,ph     = 0.025
+                  ,rng_key=None):       
 
     def SEIHR(states,t, params):
         s,e,i,h,r,c = states
@@ -38,52 +48,64 @@ if __name__ == "__main__":
 
         return np.stack([ds_dt,de_dt,di_dt,dh_dt,dr_dt, dc_dt])
 
-    N  = 12*10**6
-    
-    I0 = 5./N
-    E0 = I0
-    
-    ps = 0.10
-    S0 = 1*ps - I0 
-    
-    H0 = 0
+    #--set additional parameters for integration
+    S0 = 1*ps - I0
     R0 = 1 - S0 - I0 - E0
 
-    times = jnp.arange(0,500,1)     
-    
-    ps = 0.10
-    S0 = 1*ps - I0
+    times = jnp.arange(0,T,1)     
+    params = [sigma, (1./ps)*gamma*r0, gamma, kappa, ph]
 
-    params = [1./2, (1/ps)*(1./2)*1.4, (1./2), (1./7), 0.025 ]
-   
+    #--integrate system
     states = odeint( lambda states,t: SEIHR(states,t,params), [S0,E0,I0,H0,R0, H0], times  )
 
+    #--extract cumulative hospitalizations over time
     cum_hosps     = states[:,-1]
+
+    #--compute incident hospitalizations
     inc_hosps     = np.clip(np.diff(cum_hosps)*N, 0, np.inf)
 
+    #--add noise to inc_hosps
     noisy_hosps = np.asarray(dist.NegativeBinomial2( inc_hosps, 5.95 ).sample(rng_key))
 
+    #--compute the true peak
     time_at_peak = np.argmax(inc_hosps)
 
-    #--give training data
-    pct = 0.50
-    train_N = int(time_at_peak*pct)
+    return inc_hosps, noisy_hosps, time_at_peak
 
-    training_data =noisy_hosps[:train_N]
 
-    #--model
-    
-    def model(training_data,ttl, ps, times):
+def collect_training_data(inc_hosps,training_data,time_at_peak,pct_training_data, plus_peak=False):
+    train_N       = int(time_at_peak*pct_training_data)
+    training_data = noisy_hosps[:train_N]
+    truth_data    = inc_hosps[:train_N]
 
+    if plus_peak==False:
+        return training_data, truth_data, None
+    else:
+        total_window_of_observation = len(inc_hosps)
+        
+        training_data = np.nan*np.ones((total_window_of_observation,)) #--all nans to start
+        training_data[:train_N]     = noisy_hosps[:train_N]            #--data up to pct of peak
+        training_data[time_at_peak] = noisy_hosps[time_at_peak]        #--data AT peak
+
+        truth_data = np.nan*np.ones((total_window_of_observation,))    #--all nans to start
+        truth_data[:train_N]     = inc_hosps[:train_N]                 #--data up to pct of peak
+        truth_data[time_at_peak] = inc_hosps[time_at_peak]             #--data AT peak
+        
+        mask = ~np.isnan(training_data)
+
+        return training_data, truth_data, mask
+
+def SEIRH_Forecast(rng_key, training_data, mask, N, ps, total_window_of_observation ):
+    def model(training_data,ttl, ps, times, mask):
         #--derive from data
         T = len(training_data)
-        times = jnp.arange(0,times)
+        times = jnp.arange(0,total_window_of_observation)
         
         sigma = numpyro.sample("sigma", dist.Gamma( 1/2., 1. ) )
 
-        gamma = 1./2
-        kappa = 1./7 
-        phi   = 0.025 
+        gamma = 1./2  #--presets
+        kappa = 1./7  #--presets
+        phi   = 0.025 #--presets
 
         r0  = numpyro.sample("R0", dist.Uniform(0.75,4))
 
@@ -125,15 +147,33 @@ if __name__ == "__main__":
         inc_hosps = (states[:,-1]*ttl).reshape(-1,)
         inc_hosps = numpyro.deterministic("inc_hosps",inc_hosps)
 
-        sim = numpyro.sample("sim_inc_hosps", dist.NegativeBinomial2(inc_hosps[:T], 10), obs = training_data)
-
+        if mask is not None:
+            with numpyro.handlers.mask(mask_array=mask):
+                sim = numpyro.sample("sim_inc_hosps", dist.NegativeBinomial2(inc_hosps[:T], 10), obs = training_data)
+        else:
+            sim = numpyro.sample("sim_inc_hosps", dist.NegativeBinomial2(inc_hosps[:T], 10), obs = training_data)
+                
     nuts_kernel = NUTS(model)
     mcmc        = MCMC( nuts_kernel , num_warmup=1500, num_samples=2000,progress_bar=True)
 
-    mcmc.run(rng_key, extra_fields=('potential_energy',), training_data = training_data, ttl = N, ps = ps, times = 500)
+    mcmc.run(rng_key, extra_fields=('potential_energy',), training_data = training_data, ttl = N, ps = ps, times = times)
     mcmc.print_summary()
     samples = mcmc.get_samples()
 
+    return samples
+
+
+if __name__ == "__main__":
+    rng_key     = random.PRNGKey(0)
+    
+    inc_hosps, noisy_hosps, time_at_peak = generate_data(rng_key=rng_key, sigma = 1./7)
+    training_data, truth_data, mask = collect_training_data(inc_hosps,noisy_hosps,time_at_peak,0.50, plus_peak=True)
+    
+    N     = 12*10**6
+    ps    = 0.10
+    total_window_of_observation = 500
+    samples = SEIRH_Forecast(rng_key, training_data, mask, N, ps, total_window_of_observation ):
+    
     #--plot
     plt.scatter(times[1:train_N], noisy_hosps[1:train_N], lw=1, color="blue", alpha=1,s=3)
     plt.scatter(times[train_N+1:] , noisy_hosps[train_N:], lw=1 , color="black", alpha=1,s=3)
