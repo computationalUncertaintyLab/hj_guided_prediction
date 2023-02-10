79251828a11b136878414e5f28bc988fc3f23c55
tom mcandrew <mcandrew@lehigh.edu>
2023-02-09 21:05:10 -0500

    update

--- fig_anova_comparison_of_metrics/build_data_for_plot_factors.py
+++ fig_anova_comparison_of_metrics/build_data_for_plot_factors.py
@@ -15,15 +15,30 @@ if __name__ == "__main__":
 
     #--set randomization key
     from jax import random
-    rng_key = random.PRNGKey(0)
+    rng_key = random.PRNGKey(20180611)
 
     #--collect all forecatsing data in one file
     all_quantile_data = pd.DataFrame()
     all_peak_data     = pd.DataFrame()
+    all_pit_data      = pd.DataFrame()
+
+    #--generate two past seasons
+    past0_surv_data = generate_data(rng_key = random.PRNGKey(20120905), r0 = 1.75 + (np.random.random()-0.5) )
+    past0_inc_hosps, past0_noisy_hosps, past0_time_at_peak, past0_peak_value = past0_surv_data.simulate_surveillance_data()
+    
+    past1_surv_data = generate_data(rng_key = random.PRNGKey(20160507), r0 = 1.75 + (np.random.random()-0.5) )
+    past1_inc_hosps, past1_noisy_hosps, past1_time_at_peak, past1_peak_value = past1_surv_data.simulate_surveillance_data()
+
+    #--format past true peak data
+    s0 = pd.DataFrame({"true_time_at_peak": [past0_time_at_peak], "true_peak_value": [past0_peak_value]})
+    s1 = pd.DataFrame({"true_time_at_peak": [past1_time_at_peak], "true_peak_value": [past1_peak_value]})
+
+    past_season_peak_data = pd.concat([s0,s1])
+    past_season_peak_data.to_csv("./past_season_peak_data.csv",index=False)
     
     #--factors
-    for week in [6,5,4,3,2,1,0,-1,-2]:
-        for noise in [0.25, 1.0, 10.]:
+    for week in [4,3,2,1,-2]:
+        for noise in [2.50, 5.0, 10.]:
             for model_included in [0,1]: #--we assume that times are more similar and intesite (ie smaller variance)
 
                 #--generate simulated surveillance data
@@ -72,8 +87,8 @@ if __name__ == "__main__":
                 hj_preds.to_csv("./data/hj_preds__{:d}__{:d}__{:.2f}.csv".format(week, model_included, noise), index=False)
                 
                 #--Three models to run: just prior, surv data only, and surve plus hj
-                filenames = ["prior", "survdata", "survdata_plus_hj"]
-                options   = [(None, None, None), (cut_noisy_hosps, None, None), (cut_noisy_hosps, noisy_human_predictions, True)]
+                filenames = ["prior", "survdata","surv_data_plus_past", "survdata_plus_hj"]
+                options   = [(None, None, None), (cut_noisy_hosps, None, None),  (cut_noisy_hosps, past_season_peak_data.to_numpy(), True), (cut_noisy_hosps, noisy_human_predictions, True)]
     
                 for f,o in zip(filenames,options) :
                     d, h, o = o #--these are the options that control what information gets to the model
@@ -98,6 +113,24 @@ if __name__ == "__main__":
 
                     pickle.dump( forecast, open("./plotdata/{:s}__forecast__{:d}__{:d}__{:.2f}.pkl".format(f, week, model_included, noise), "wb"))
 
+                    #--compute PITS from X weeks before to 4 weeks after
+
+                    #--compute the proportion of times the sample posterior is smaller than the truth
+                    nsamples = samples["inc_hosps"].shape[0]
+                    long_samples          = samples["inc_hosps"].T.flatten()
+                    repeated_times        = np.arange(0,210).repeat(nsamples) 
+
+                    long_samples = pd.DataFrame({"sample":long_samples,"times":repeated_times})
+
+                    #--merge in truth
+                    data_ready_for_pit = long_samples.merge(truthdata, on = ["times"])
+
+                    def PIT(x):
+                        return pd.Series({"PIT": np.mean(x["sample"]<x["hosps"]) })
+                    PITS_for_incident_hosps = data_ready_for_pit.groupby(["times"]).apply(PIT)
+                    
+                    PITS_for_incident_hosps.to_csv("./plotdata/{:s}__PITS__{:d}__{:d}__{:.2f}.csv".format(f, week, model_included, noise), index=False)
+
                     #--add to large dfs
                     quantiles_for_incident_hosps["week"]           = week
                     quantiles_for_incident_hosps["noise"]          = noise
@@ -112,5 +145,15 @@ if __name__ == "__main__":
                     peaks_and_times["model"]          = f
  
                     all_peak_data = pd.concat([all_peak_data, peaks_and_times])
+
+
+                    PITS_for_incident_hosps["week"]           = week
+                    PITS_for_incident_hosps["noise"]          = noise
+                    PITS_for_incident_hosps["model_included"] = model_included
+                    PITS_for_incident_hosps["model"]          = f
+                    
+                    all_pit_data = pd.concat([all_pit_data, PITS_for_incident_hosps])
+                    
     all_quantile_data.to_csv("./plotdata/all_quantile_data.csv",index=False)
     all_peak_data.to_csv("./plotdata/all_peak_data.csv",index=False)
+    all_pit_data.to_csv("./plotdata/all_pit_data.csv",index=False)
--- fig_anova_comparison_of_metrics/plot.py
+++ fig_anova_comparison_of_metrics/plot.py
@@ -6,7 +6,135 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import scienceplots
+
+def mm2inch(x):
+    return x/25.4
+
+def stamp(ax,s):
+    ax.text(0.0125,0.975,s=s,fontweight="bold",fontsize=10,ha="left",va="top",transform=ax.transAxes)
+
 if __name__ == "__main__":
 
-    
+    all_quantile_data = pd.read_csv("./plotdata/all_quantile_data.csv")
+    all_peak_data     = pd.read_csv("./plotdata/all_peak_data.csv")
+   
+    true_data = pd.read_csv("../for_crowdsourcing/data_collection__0__0__10.00/truth_data__0__0__10.00.csv")
+    true_peak = int(pd.read_csv("../for_crowdsourcing/data_collection__0__0__10.00/time_at_peak__0__0__10.00.csv").time_at_peak)
+    true_peak_value = true_data.truth_data.values[true_peak]
 
+    all_peak_data.loc[all_peak_data.model != "prior"]
+
+    
+    #--peak MAE, peak PIT timing
+    #--peak MAE, peak PIT intensiyt
+
+    all_peak_data["true_peak_time"]  = true_peak
+    all_peak_data["true_peak_value"] = true_peak_value
+
+    all_peak_data["diff_time"]  = all_peak_data["times"] - all_peak_data["true_peak_time"]
+    all_peak_data["diff_value"] = all_peak_data["peaks"] - all_peak_data["true_peak_value"]
+
+    all_peak_data["abs_diff_time"]  = abs(all_peak_data["times"] - all_peak_data["true_peak_time"])
+    all_peak_data["abs_diff_value"] = abs(all_peak_data["peaks"] - all_peak_data["true_peak_value"])
+
+    all_peak_data["model"] = all_peak_data["model"].replace("prior","No data")
+    all_peak_data["model"] = all_peak_data["model"].replace("survdata","Surveillance data")
+    all_peak_data["model"] = all_peak_data["model"].replace("survdata_plus_hj","Surveillance plus HJ")
+    
+    plt.style.use(['science','grid'])
+    
+    fig,axs = plt.subplots(2,3)
+
+    ax = axs[0,0]
+    sns.boxplot(hue="model", y="diff_time", x="week"          , fliersize=0, data = all_peak_data, ax=ax)
+    ax.set_ylabel("Post. samples\nminus true peak day", fontsize=10)
+    ax.set_xlabel("", fontsize=10)
+
+    ax.legend(frameon=False, loc = "upper right", labelspacing = 0.05)
+    stamp(ax,"A.")
+    
+    ax = axs[0,1]
+    sns.boxplot(hue="model", y="diff_time", x="model_included", fliersize=0, data = all_peak_data, ax=ax)
+    ax.set_ylabel("", fontsize=10)
+
+    ax.get_legend().remove()
+     
+    ax.set_xlabel("", fontsize=10)
+    ax.set_xticklabels(["No model guidance", "Model\nguidance"], fontsize=10)
+
+    stamp(ax,"B.")
+    
+    ax = axs[0,2]
+    sns.boxplot(hue="model", y="diff_time", x="noise"         , fliersize=0, data = all_peak_data, ax=ax)
+
+    ax.set_ylabel("", fontsize=10)
+
+    ax.set_xlabel("", fontsize=10)
+    ax.set_xticklabels(["High\nnoise", "Medium", "Low"], fontsize=10)
+
+    ax.get_legend().remove()
+
+    stamp(ax,"C.")
+
+    #--intensity
+    ax = axs[1,0]
+    sns.pointplot(hue="model", y="diff_value", x="week", data = all_peak_data, ax=ax)
+    ax.set_ylabel("Post. samples\nminus true peak intensity", fontsize=10)
+    ax.set_xlabel("", fontsize=10)
+
+    ax.set_ylim(-500,1500)
+    ax.get_legend().remove()
+    stamp(ax,"D.")
+
+    ax = axs[1,1]
+    sns.pointplot(hue="model", y="diff_value", x="model_included", data = all_peak_data, ax=ax)
+    ax.set_ylabel("", fontsize=10)
+
+    ax.get_legend().remove()
+     
+    ax.set_xlabel("", fontsize=10)
+    ax.set_xticklabels(["No model guidance", "Model\nguidance"], fontsize=10)
+
+    ax.set_ylim(-500,1500)
+    stamp(ax,"E.")
+    
+    ax = axs[1,2]
+    sns.pointplot(hue="model", y="diff_value", x="noise", data = all_peak_data, ax=ax)
+
+    ax.set_ylabel("", fontsize=10)
+
+    ax.set_xlabel("", fontsize=10)
+    ax.set_xticklabels(["High\nnoise", "Medium", "Low"], fontsize=10)
+
+    ax.get_legend().remove()
+
+
+    ax.set_ylim(-500,1500)
+    stamp(ax,"F.")
+    
+    fig.set_tight_layout(True)
+    w = mm2inch(183)
+
+    fig.set_size_inches(w, w/1.5)
+
+    plt.savefig("./anova_on_peak_timing_and_intensity.pdf")
+    plt.close()
+
+    
+    #--setup two anovas
+    import statsmodels.formula.api as smf
+    
+    mod = smf.ols(formula='abs_diff_time ~ C(week) + C(model_included) + C(noise)-1', data=all_peak_data)
+    res = mod.fit()
+    print(res.summary())
+
+
+    mod = smf.ols(formula='abs_diff_value ~ C(week) + C(model_included) + C(noise)-1', data=all_peak_data)
+    res = mod.fit()
+    print(res.summary())
+
+
+
+
+    
